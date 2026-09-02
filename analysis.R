8bb5733e7d01049af9893c905dc2a806468e121f
yuhej-blip <yuhej@umich.edu>
2026-09-02 15:07:32 -0400

    Calculate mean score for public data

--- analysis.R
+++ analysis.R
@@ -5,3 +5,4 @@ public_data <- read.csv("data/public_data.csv")
 # Your task: calculate the mean score for the public data.
 # Add your code here on your own branch.
 
+mean_score <- mean(public_data$score)
